d6e8bfaa5f4b1f22b809b0edeb9c8b933e050589
Piraset-Nobran <piraset.jan@gmail.com>
2025-05-16 19:21:26 +0700

    .

--- app.js
+++ app.js
@@ -1,14 +1,54 @@
+/*import chalk from 'chalk';
+import express from 'express';
+import debug from 'debug';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+const port = process.env.Port || 4000;
+
+app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")));
+//app.set("view","./src/view/index.ejs");
+app.set("view", path.join(__dirname, "src/view"));
+app.set("view engine","ejs");
+app.get('/',(req,res)=>{
+    res.render('index',{username:'ManowMoosub'});
+});
+app.listen(port,()=>{
+    console.log(chalk.green(`Listening on port: ${port}`));
+})*/
 import chalk from 'chalk';
 import express from 'express';
 import debug from 'debug';
 import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
-const port = 2000;
+const port = process.env.Port || 4000;
+const productRouter = express.Router();
 
 app.use(morgan('combined'));
-app.get('/',(req,res)=>{
-    res.send('Hello world');
+app.use(express.static(path.join(__dirname, "/public/")));
+app.use("/products", productRouter)
+app.set("views", path.join(__dirname, "src/view"));
+app.set("view engine", "ejs");
+productRouter.route("/").get((req, res) => {
+    res.send("Hello this is Product ");
 });
-app.listen(port,()=>{
-    console.log("Lisetening On port %d",chalk.red(" : "+port));
-})
+productRouter.route("/1").get((req, res) => {
+    res.send("Hello this is Product 1");
+});
+
+app.get('/', (req, res) => {
+    res.render('index', { username: 'ManowMoosub', customer: ['Manow', 'Moosub', 'Padped'] });
+});
+
+app.listen(port, () => {
+    console.log(chalk.green(`Listening on port: ${port}`));
+});
